3c91a0348a3f27d3d119b6e2daff01105aea5ee3
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-06-25 15:20:27 +0200

    I'm finding myself failing with staging commit. However, there's the new logger file.

---
+++ src/com/example/app_android/Logger.java
@@ -0,0 +1,18 @@
+package com.example.app_android;
+
+import android.util.Log;
+
+public final class Logger {
+
+	private final static boolean verbose = true;
+	
+	private Logger() {
+		
+	}
+	
+	static void VerboseLog(String tag, String message)
+	{
+		if(verbose)
+			Log.v(tag, message);
+	}
+}
