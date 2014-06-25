479ffae3abcf2fbf18f01f3c18b3f30ec7ccd5d0
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-06-25 16:02:31 +0200

    Changed name on everything to make it easier to read.

--- src/com/example/app_android/ListSelectionListener.java
+++
@@ -1,6 +0,0 @@
-package com.example.app_android;
-
-// Interface for communication between fragments and activities
-public interface ListSelectionListener {
-	public void onListSelection(int index);
-}
