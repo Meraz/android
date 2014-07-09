822306caee9cf533af18703d73b1b2431cf3b291
Daniel Bengtsson <Daniel-Bengtsson@live.com>
2014-07-09 17:23:13 +0200

    Prepared and reworked schedule code to work with google calendar

--- src/com/example/app_android/ActivityMyCoursesAndProgram.java
+++ src/com/example/app_android/ActivityMyCoursesAndProgram.java
@@ -1,15 +1,35 @@
 package com.example.app_android;
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.TimeZone;
+
 import com.example.app_android.InterfaceListSelectionListener;
+import com.example.app_android.AdapterScheduleHelper.MyScheduleHelper;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class ActivityMyCoursesAndProgram extends Activity implements InterfaceListSelectionListener{
+public class ActivityMyCoursesAndProgram extends Activity implements InterfaceListSelectionListener {
 
 	private static final String TAG = "ActivityCoursesAndProgram";
 	public static String[] coursesAndProgramArray;
@@ -21,9 +41,16 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mycoursesandprogram);
+		
 		courseCode = (EditText) findViewById(R.id.courseCode);
 		coursesHelper = new AdapterCoursesHelper(this);
 		coursesAndProgramArray = coursesHelper.readAllCourses();
+		
+		FetchTimeEditDataTask dataFetchTask = new FetchTimeEditDataTask();
+		dataFetchTask.execute("https://se.timeedit.net/web/bth/db1/sched1/s.csv?tab=5&object=dv2544&type=root&startdate=20140101&enddate=20140620&p=0.m%2C2.w");
+    	//Intent intent = new Intent(this, ServiceSchemaUpdate.class);
+    	//intent.putExtra("URL", "https://se.timeedit.net/web/bth/db1/sched1/s.csv?tab=5&object=dv2544&type=root&startdate=20140101&enddate=20140620&p=0.m%2C2.w");
+    	//startService(intent);
 	}
 	
     @Override
@@ -75,6 +102,10 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 		}		
 	}
 	
+	public void exportSchedule(View view) {
+		Logger.VerboseLog(TAG, "Exporting schedule to Google Calendar");
+	}
+	
 	public void readCourses() {
 		coursesAndProgramArray = coursesHelper.readAllCourses();
 	}
@@ -87,4 +118,132 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 	public void onListSelection(int index) {
 		// TODO Auto-generated method stub
 	}
+	
+	private ArrayList<String[]> getTimeEditData(String stringURL) {
+		String inputLine;
+		ArrayList<String[]> lectures = new ArrayList<String[]>();
+		try {
+			URL url = new URL(stringURL);
+			HttpURLConnection urlCon = (HttpURLConnection)url.openConnection();
+			InputStream inStream = urlCon.getInputStream();
+			BufferedReader readBuff = new BufferedReader(new InputStreamReader(inStream));
+
+			int count = 0;
+			while((inputLine = readBuff.readLine()) != null) {
+				if(count > 6) {
+					String[] tokens = inputLine.split(",");
+					String[] lecture =  parseTimeEditData(tokens);
+					lectures.add(lecture);
+				}
+				count++;
+			}			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lectures;
+	}
+	
+	private String[] parseTimeEditData(String[] tokens) {
+		String[] stringParts = new String[10];
+		
+		stringParts[0] = tokens[0]; //Start date
+		stringParts[1] = tokens[1].substring(1); //Start time
+		stringParts[2] = tokens[2].substring(1); //End date
+		stringParts[3] = tokens[3].substring(1); //End time
+		
+		
+		
+		int index = 4;
+		
+		if(tokens[index].indexOf('"') == 1) { //Course
+			stringParts[4] = tokens[index++] + tokens[index++];
+		} else {
+			stringParts[4] = tokens[index++];
+		}
+		if(tokens[index].indexOf('"') == 1) {	//? TODO Check what this should contain
+			stringParts[5] = tokens[index++] + tokens[index++];
+		} else {
+			stringParts[5] = tokens[index++];
+		}
+		if(tokens[index].indexOf('"') == 1) { //Room
+			stringParts[6] = tokens[index++] + tokens[index++];
+		} else {
+			stringParts[6] = tokens[index++];
+		}
+		if(tokens[index].indexOf('"') == 1) { //Booking person
+			stringParts[7] = tokens[index++] + tokens[index++];
+		} else {
+			stringParts[7] = tokens[index++];
+		}
+		if(tokens[index].indexOf('"') == 1) {	//Info
+			stringParts[8] = tokens[index++] + tokens[index++];
+		} else {
+			stringParts[8] = tokens[index++];
+		}
+		if(tokens[index].indexOf('"') == 1) {	//Group
+			stringParts[9] = tokens[index++] + tokens[index++];
+		} else {
+			stringParts[9] = tokens[index++];
+		}
+		return stringParts;
+	}
+	
+	private void exportScheduleEvent(String[] eventData) {
+		//Initialize data
+		long startTimeMillis = 0;
+		long endTimeMillis = 0;
+		String[] startDateParts = eventData[0].split("-");
+		String[] startTimeParts = eventData[1].split(":");
+		String[] endDateParts 	= eventData[2].split("-");
+		String[] endTimeParts 	= eventData[3].split(":");
+		Uri uri = CalendarContract.Calendars.CONTENT_URI;
+		String[] projection = new String[] {
+		       CalendarContract.Calendars._ID,
+		       CalendarContract.Calendars.ACCOUNT_NAME,
+		       CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+		       CalendarContract.Calendars.NAME,
+		       CalendarContract.Calendars.CALENDAR_COLOR
+		};
+		
+		//Get start and end time in milliseconds
+		Calendar beginTime = Calendar.getInstance();
+		beginTime.set(Integer.parseInt(startDateParts[0]), Integer.parseInt(startDateParts[1]), Integer.parseInt(startDateParts[2]),
+				Integer.parseInt(startTimeParts[0]), Integer.parseInt(startTimeParts[1]));
+		startTimeMillis = beginTime.getTimeInMillis();
+		
+		Calendar endTime = Calendar.getInstance();
+		endTime.set(Integer.parseInt(endDateParts[0]), Integer.parseInt(endDateParts[1]), Integer.parseInt(endDateParts[2]),
+				Integer.parseInt(endTimeParts[0]), Integer.parseInt(endTimeParts[1]));
+		endTimeMillis  = endTime.getTimeInMillis();
+		
+		//Insert the event
+		ContentResolver contentResolver = getContentResolver();
+		ContentValues values = new ContentValues();
+		TimeZone timeZone = TimeZone.getDefault();
+		values.put(CalendarContract.Events.DTSTART, startTimeMillis);
+		values.put(CalendarContract.Events.DTEND, endTimeMillis);
+		values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
+		values.put(CalendarContract.Events.TITLE, eventData[4] + eventData[8] + eventData[7] + eventData[6]);
+		values.put(CalendarContract.Events.DESCRIPTION, "The storm troopers are coming and they are bringing cheese"); //TODO set relevant description
+		values.put(CalendarContract.Events.CALENDAR_ID, 0);
+		uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
+	}
+	
+	private class FetchTimeEditDataTask extends AsyncTask<String, Void, Void> {
+
+		@Override
+		protected Void doInBackground(String... params) {
+			ArrayList<String[]> lectureList = getTimeEditData(params[0]);
+			for(int i = 0; i < lectureList.size(); ++i) {
+				exportScheduleEvent(lectureList.get(i));
+			}
+			return null;
+		}
+	}
+	
+	
 }
