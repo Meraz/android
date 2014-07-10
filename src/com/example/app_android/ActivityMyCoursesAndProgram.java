6ffa28b69a90efb0b7c7c64c35a7ac75415af12a
Daniel Bengtsson <Daniel-Bengtsson@live.com>
2014-07-10 12:23:00 +0200

    Adding events to google calendar from single time edit course is now working. Not connected to the UI though.

--- src/com/example/app_android/ActivityMyCoursesAndProgram.java
+++ src/com/example/app_android/ActivityMyCoursesAndProgram.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Calendar;
 import java.util.List;
@@ -20,6 +21,7 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -155,8 +157,6 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 		stringParts[2] = tokens[2].substring(1); //End date
 		stringParts[3] = tokens[3].substring(1); //End time
 		
-		
-		
 		int index = 4;
 		
 		if(tokens[index].indexOf('"') == 1) { //Course
@@ -193,23 +193,16 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 	}
 	
 	private void exportScheduleEvent(String[] eventData) {
-		//Initialize data
+		int[] calendarIDs = null;
 		long startTimeMillis = 0;
 		long endTimeMillis = 0;
+		
+		//Get start and end time in milliseconds
 		String[] startDateParts = eventData[0].split("-");
 		String[] startTimeParts = eventData[1].split(":");
 		String[] endDateParts 	= eventData[2].split("-");
 		String[] endTimeParts 	= eventData[3].split(":");
-		Uri uri = CalendarContract.Calendars.CONTENT_URI;
-		String[] projection = new String[] {
-		       CalendarContract.Calendars._ID,
-		       CalendarContract.Calendars.ACCOUNT_NAME,
-		       CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
-		       CalendarContract.Calendars.NAME,
-		       CalendarContract.Calendars.CALENDAR_COLOR
-		};
 		
-		//Get start and end time in milliseconds
 		Calendar beginTime = Calendar.getInstance();
 		beginTime.set(Integer.parseInt(startDateParts[0]), Integer.parseInt(startDateParts[1]), Integer.parseInt(startDateParts[2]),
 				Integer.parseInt(startTimeParts[0]), Integer.parseInt(startTimeParts[1]));
@@ -220,8 +213,25 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 				Integer.parseInt(endTimeParts[0]), Integer.parseInt(endTimeParts[1]));
 		endTimeMillis  = endTime.getTimeInMillis();
 		
-		//Insert the event
+		//Get the calendar ID
 		ContentResolver contentResolver = getContentResolver();
+		String[] projection = new String[] {
+		       CalendarContract.Calendars._ID,
+		       CalendarContract.Calendars.ACCOUNT_NAME,
+		       CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,
+		       CalendarContract.Calendars.NAME,
+		       CalendarContract.Calendars.CALENDAR_COLOR
+		};
+		Cursor cursor = contentResolver.query(Uri.parse("content://com.android.calendar/calendars"), projection, null, null, null);
+		if (cursor.moveToFirst()) {
+			calendarIDs = new int[cursor.getCount()];
+            for (int i = 0; i < calendarIDs.length; i++) {
+            	calendarIDs[i] = cursor.getInt(0);
+                cursor.moveToNext();
+            }
+        }
+		
+		//Insert the event
 		ContentValues values = new ContentValues();
 		TimeZone timeZone = TimeZone.getDefault();
 		values.put(CalendarContract.Events.DTSTART, startTimeMillis);
@@ -229,12 +239,11 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 		values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
 		values.put(CalendarContract.Events.TITLE, eventData[4] + eventData[8] + eventData[7] + eventData[6]);
 		values.put(CalendarContract.Events.DESCRIPTION, "The storm troopers are coming and they are bringing cheese"); //TODO set relevant description
-		values.put(CalendarContract.Events.CALENDAR_ID, 0);
-		uri = contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
+		values.put(CalendarContract.Events.CALENDAR_ID, calendarIDs[1]);
+		contentResolver.insert(CalendarContract.Events.CONTENT_URI, values);
 	}
 	
 	private class FetchTimeEditDataTask extends AsyncTask<String, Void, Void> {
-
 		@Override
 		protected Void doInBackground(String... params) {
 			ArrayList<String[]> lectureList = getTimeEditData(params[0]);
@@ -244,6 +253,5 @@ public class ActivityMyCoursesAndProgram extends Activity implements InterfaceLi
 			return null;
 		}
 	}
-	
 	
 }
