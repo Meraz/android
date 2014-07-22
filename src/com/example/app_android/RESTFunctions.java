6b722821dc3e27c66932502f85afb98413e3c116
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-07-22 17:07:20 +0200

    Cleanded up some old code and added a overview list on what to do for REST token handling

--- src/com/example/app_android/RESTFunctions.java
+++ src/com/example/app_android/RESTFunctions.java
@@ -13,6 +13,8 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
 import android.os.AsyncTask;
 
 public final class RESTFunctions {
@@ -22,151 +24,86 @@ public final class RESTFunctions {
 		
 	}
 	
-	public static String doSendRequest(final String adress, final String message) {
-
-		return new String("hej");
-	}
-	
-	/*
 	// Do a "Get request" according to the REST functionality.
 	// returns "MalformedURLException" for the application to handle
-	public static String doGetRequest(final String adress, final String message) throws MalformedURLException {
+	public static String doGetRequest(final String url, final String[] parameterNames, final String[] parameterValues) {
 		
-		URL url = new URL(adress);		
-		HttpURLConnection conn = null;
-		BufferedReader reader = null;
 		
+		/* REST TOKEN
+		 * 1. Build message; header, url and if needed some body information
+		 * 2. (Send data) Receive result and take appropirate action
+		 * 3. (if success) Parse data
+		 * 3.1 Extract data from headers. (Token, and expire time.
+		 * 4. Save appropirate data in database. Token and expire date
+		 * 4.1 Calculate expire data by doing. currenttime += expires_in
+		 * 
+
+		 */
+		
+		String fetchResult = "";			
+		//fetchResult = readRequest2();
+	//	fetchResult = task.get();
+
+		
+		return fetchResult;
+	}
+	
+	private JSONObject convertHeadersToJSON(Header[] all_headers) {
+		return new JSONObject();
+	}
+	
+	public String readRequest2(String request) {
+		StringBuilder stringBuilder = new StringBuilder();
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost(request);
+
 		try {
-			conn = (HttpURLConnection) url.openConnection();
-			//conn.setRequestMethod("GET");
-			//conn.setRequestProperty("Accept", "application/json");
-			//conn.setRequestMethod(message);
+			HttpResponse response = httpClient.execute(httpPost);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
 			
-			reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-		} catch (IOException e) {
-			// TODO
-			e.printStackTrace();
-		}
-				  
-		String line;
-		String result = "";			 
-        try {
-			while ((line = reader.readLine()) != null) {
-			   result += line;
+			Header[] all_headers = response.getAllHeaders();
+			
+			if(statusCode == 200) {				
+				HttpEntity httpEntity = response.getEntity();
+				InputStream content = httpEntity.getContent();
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while((line = bufferedReader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+			} else {
+				//					Log.e(RoomActivity.class.toString(), "Failed to download file!");
 			}
-			reader.close();
-		} catch (IOException e) {
-			// TODO
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
-		return result;
+		return stringBuilder.toString();
 	}
-	*/
 	
-	// Do a "Get request" according to the REST functionality.
-	// returns "MalformedURLException" for the application to handle
-		public static String doGetRequest(final String adress, final String message) {
-			
-			String fetchResult = "";			
-	    	DataFromWeb task = new DataFromWeb();
-	    	task.execute(adress);
-			try {
-				fetchResult = task.get();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} catch (ExecutionException e) {
-				e.printStackTrace();
-			}
-			
-			return fetchResult;
-		}
-		
-		private static class DataFromWeb extends AsyncTask<String, Void, String> {
-
-			@Override
-			protected void onPreExecute() {
-				// TODO 
-				super.onPreExecute();
-			}
-
-			public String readRequest(String request) {
-				StringBuilder stringBuilder = new StringBuilder();
-				HttpClient httpClient = new DefaultHttpClient();
-				HttpGet httpGet = new HttpGet(request);
-
-				try {
-					HttpResponse response = httpClient.execute(httpGet);
-					StatusLine statusLine = response.getStatusLine();
-					int statusCode = statusLine.getStatusCode();
-					if(statusCode == 200) {
-						HttpEntity httpEntity = response.getEntity();
-						InputStream content = httpEntity.getContent();
-						BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
-						String line;
-						while((line = bufferedReader.readLine()) != null) {
-							stringBuilder.append(line);
-						}
-					} else {
-						//					Log.e(RoomActivity.class.toString(), "Failed to download file!");
-					}
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-				return stringBuilder.toString();
-			}
-			
-			public String readRequest2(String request) {
-				StringBuilder stringBuilder = new StringBuilder();
-				HttpClient httpClient = new DefaultHttpClient();
-				HttpPost httpPost = new HttpPost(request);
-
-				try {
-					HttpResponse response = httpClient.execute(httpPost);
-					StatusLine statusLine = response.getStatusLine();
-					int statusCode = statusLine.getStatusCode();
-					
-					Header[] all = response.getAllHeaders();
-					
-					
-					Header access_token = response.getFirstHeader("access_token");
-					Header token_type = response.getFirstHeader("token_type");
-					Header expires_in = response.getFirstHeader("expires_in");
-					String a = expires_in.toString();
-					
-					if(statusCode == 200) {
-						HttpEntity httpEntity = response.getEntity();
-						InputStream content = httpEntity.getContent();
-						BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
-						String line;
-						while((line = bufferedReader.readLine()) != null) {
-							stringBuilder.append(line);
-						}
-					} else {
-						//					Log.e(RoomActivity.class.toString(), "Failed to download file!");
-					}
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
-				return stringBuilder.toString();
-			}
-			
-			
-			@Override
-			protected String doInBackground(String... params) {				
-				String httpResponse = readRequest2("http://194.47.131.73/database-files-and-server-script/Script/serverResponse.php");
-				return httpResponse;
-				//   return new String("hej");
-			}
-				
-			protected void onPostExecute(String result) {
-				// TODO
-				super.onPostExecute(result);
-			}		
-		}
+	public String readRequest(String request) {
+		StringBuilder stringBuilder = new StringBuilder();
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(request);
 	
-	public static String doPutRequest(final String adress, final String message)	{
-		
-		return new String("hej");
-	}		
+		try {
+			HttpResponse response = httpClient.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if(statusCode == 200) {
+				HttpEntity httpEntity = response.getEntity();
+				InputStream content = httpEntity.getContent();
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while((line = bufferedReader.readLine()) != null) {
+					stringBuilder.append(line);
+				}
+			} else {
+				//					Log.e(RoomActivity.class.toString(), "Failed to download file!");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return stringBuilder.toString();
+	}
 }
