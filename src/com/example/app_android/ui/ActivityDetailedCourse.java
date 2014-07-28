d0655c07134e392cd8ce2379d5edd14c7083fc71
Daniel Bengtsson <Daniel-Bengtsson@live.com>
2014-07-28 16:06:49 +0200

    Inverted the behaviour of the icon on the favourites toggle in the detailed course view. Also added toasts to inform the user about the result of their request to favourite/defavourite a course

--- src/com/example/app_android/ui/ActivityDetailedCourse.java
+++ src/com/example/app_android/ui/ActivityDetailedCourse.java
@@ -105,15 +105,19 @@ public class ActivityDetailedCourse extends Activity {
 	    case R.id.detailed_course_action_add_or_remove:
 	    	if(isFavourite) {
 	    		isFavourite = false;
-	    		if(coursesHelper.removeCourse(courseCode))
-	    			addOrRemoveButton.setIcon(R.drawable.ic_action_not_important);
+	    		if(coursesHelper.removeCourse(courseCode)) {
+	    			addOrRemoveButton.setIcon(R.drawable.ic_action_important);
+	    			Toast.makeText(getApplicationContext(), courseCode + " was added to favourite courses", Toast.LENGTH_SHORT).show();
+	    		}
 	    		else
 	    			Toast.makeText(getApplicationContext(), "Failed to remove course :(", Toast.LENGTH_SHORT).show();
 	    	}
 	    	else {
 	    		isFavourite = true;
-	    		if(coursesHelper.insertData(courseCode) >= 0)	//id >= 0 = success
-	    			addOrRemoveButton.setIcon(R.drawable.ic_action_important);
+	    		if(coursesHelper.insertData(courseCode) >= 0) {	//id >= 0 = success
+	    			addOrRemoveButton.setIcon(R.drawable.ic_action_not_important);
+	    			Toast.makeText(getApplicationContext(), courseCode + " was removed from favourite courses", Toast.LENGTH_SHORT).show();
+	    		}
 	    		else
 	    			Toast.makeText(getApplicationContext(), "Failed to add course :(", Toast.LENGTH_SHORT).show();
 	    	}
