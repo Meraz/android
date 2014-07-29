81a1851635ee2d5e5789dadd8024a2e28eea639e
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-07-29 17:16:06 +0200

    More works on request token function, wrote some better error messesages and completed time management. Still hardcoded values here and there. One step at the time

--- src/com/example/app_android/rest/Processes.java
+++ src/com/example/app_android/rest/Processes.java
@@ -30,8 +30,21 @@ public class Processes {
 		
 		tokenTable.updateTransactionFlag(1); // Update to pending transaction flag.  TODO, use enumerations
 		
-		HttpPost httpPost = null;
-		httpPost = preparePostCall("http://194.47.131.73/database-files-and-server-script/Script/serverResponse.php", null, null);
+		HttpPost httpPost = null;		
+		
+		String[] parameterName 	 = new String[4];
+		parameterName[0] = "grant_type";
+		parameterName[1] = "username";								
+		parameterName[2] = "password";						
+		parameterName[3] = "client_id";
+
+		String[] parameterValues = new String[4];
+		parameterValues[0] = "password";								// TODO hardcoded
+		parameterValues[1] = "npr";										// TODO hardcoded
+		parameterValues[2] = "secret_password";							// TODO hardcoded
+		parameterValues[3] = "21EC2020-3AEA-1069-A2DD-08002B30309D";	// TODO hardcoded
+
+		httpPost = preparePostCall("http://194.47.131.73/database-files-and-server-script/Script/serverResponse.php", parameterName, parameterValues);
 		
 		HttpResponse httpResponse = null;
 		httpResponse = RESTFunctions.requestToken(httpPost);		
@@ -39,45 +52,32 @@ public class Processes {
 		int statusCode = httpResponse.getStatusLine().getStatusCode();
 		if(statusCode != 200) {			
 			tokenTable.updateTransactionFlag(-1); 				// Update to fail transaction flag. TODO, use enumerations
-			throw new RestCommunicationException("Could not access server, error message: " + statusCode);
+			throw new RestCommunicationException("Could not access server. HTTPCODE: " + statusCode);
 		}				
 		
 		String access_token = httpResponse.getFirstHeader("access_token").getValue();				// Get token value from header
-		int expires_in = Integer.parseInt(httpResponse.getFirstHeader("expires_in").getValue());	// Get expireDate from header // TODO, convert from whatever to whatever; Needs to decide how this should work.
+		Long expires_in = Long.parseLong(httpResponse.getFirstHeader("expires_in").getValue());	// Get expireDate from header // TODO, convert from whatever to whatever; Needs to decide how this should work.
 		if(access_token == null || expires_in == 0) {
 			throw new RestCommunicationException("Server returned bad data.");
 		}		
 		
-		//	expires_in += getCurrentTime();																// Add currentTime for expire_time
-
+		expires_in += (System.currentTimeMillis()/1000);					// Add currentTime for expire_time
+		tokenTable.PrintEntireToken();
 		tokenTable.updateToken(access_token, expires_in, 0);										// updateExistingToken // TODO use enumerations for transaction flag
+		tokenTable.PrintEntireToken();
 	}
 	
 	private static HttpPost preparePostCall(String url, final String[] parameterNames, final String[] parameterValues) throws RestCommunicationException {
 		
 		HttpPost httpPost = new HttpPost(url);
-		
-		/*
+				
 		if(parameterNames.length != parameterValues.length)
-			throw new RestCommunicationException("Not the same amount of parameters as there is values");
+			throw new RestCommunicationException("Not the same amount of parameters as there is values.");
 		
 		
 		for(int i = 0; i < parameterNames.length; i++) {
 			httpPost.addHeader(parameterNames[i], parameterValues[i]);
 		}
-		*/
-		
-		 // TODO. IT'S HARDCODED ATM
-		
-		String grant_type = new String("password");
-		String username = new String("npr");
-		String password = new String("secret_password");
-		String client_id = new String("21EC2020-3AEA-1069-A2DD-08002B30309D");
-		
-		httpPost.addHeader("grant_type", grant_type);
-		httpPost.addHeader("username", username);
-		httpPost.addHeader("password", password);
-		httpPost.addHeader("client_id", client_id);
 		return httpPost;
 	}
 
