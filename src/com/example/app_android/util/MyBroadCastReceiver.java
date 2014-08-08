095aa92597a80f188e66c149c14e5bace1e03779
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-08-08 11:14:43 +0200

    Added logging in 'MyBroadCastReceiver'

--- src/com/example/app_android/util/MyBroadCastReceiver.java
+++ src/com/example/app_android/util/MyBroadCastReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.util.Log;
 
 public class MyBroadCastReceiver extends BroadcastReceiver {
 	
@@ -20,16 +21,20 @@ public class MyBroadCastReceiver extends BroadcastReceiver {
 	}
 
 	private static final String TAG = "Callback";
-	private String mStartBroadCast = null;
-	private String mStopBroadCast = null;
-	private String mUpdateBroadCast = null;
-	private Receiver mReceiver = null;
+	private String mClassName; 
+	
+	protected String mStartBroadCast = null;
+	protected String mStopBroadCast = null;
+	protected String mUpdateBroadCast = null;
+	protected Receiver mReceiver = null;
 	
 	public MyBroadCastReceiver(String startBroadCast, String updateBroadCast, String stopBroadCast)
 	{
 		mStartBroadCast 	= startBroadCast;
 		mUpdateBroadCast 	= updateBroadCast;
 		mStopBroadCast 		= stopBroadCast;
+		
+		mClassName = getClass().getSimpleName();
 	}
 	
 	public MyBroadCastReceiver(String stopBroadCast)
@@ -39,10 +44,12 @@ public class MyBroadCastReceiver extends BroadcastReceiver {
 	
 	public void registerCallback(Receiver receiver)
 	{
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":registerCallback()");}
 		mReceiver = receiver;
 	}
 	
 	public void registerBroadCastReceiver(Context context) {
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":registerBroadCastReceiver()");}
 		IntentFilter filter = new IntentFilter();
 		if(mStartBroadCast != null)
 		{
@@ -62,31 +69,36 @@ public class MyBroadCastReceiver extends BroadcastReceiver {
 	}
 	
 	public void unregisterBroadCastReceiver(Context context) {
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":unregisterBroadCastReceiver()");}
 		context.unregisterReceiver(this);
 	}
 	
 	public String getStartBroadCast()
 	{
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":getStartBroadCast()");}
 		return mStartBroadCast;
 	}
 	
 	public String getUpdateBroadCast()
 	{
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":getUpdateBroadCast()");}
 		return mUpdateBroadCast;
 	}
 	
 	public String getStopBroadCast()
 	{
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":getStopBroadCast()");}
 		return mStopBroadCast;
 	}
 	
-	public String getAllBroadcasts() {
+	public String getAllBroadcastStringAsOne() {
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":getAllBroadcastStringAsOne()");}
 		return mStartBroadCast+mStopBroadCast+mUpdateBroadCast;
 	}
 	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Utilities.VerboseLog(TAG, getClass().getSimpleName() + ":entered onReceive()");
+		if(Utilities.verbose) {Log.v(TAG, mClassName + ":onReceive()");}
 
 		if(mReceiver != null) {
 			String action = intent.getAction();
