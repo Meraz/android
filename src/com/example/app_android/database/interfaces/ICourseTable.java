4d2f98ef30111c105e3c70e29d9e7f95976335fc
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-08-11 16:23:03 +0200

    Added todo flags

--- src/com/example/app_android/database/interfaces/ICourseTable.java
+++ src/com/example/app_android/database/interfaces/ICourseTable.java
@@ -9,11 +9,23 @@ import com.example.app_android.database.NoRowsAffectedDBException;
 
 public interface ICourseTable {
 	
+	/*
+	 * TODO
+	 */
 	public boolean add(CourseBean course) throws DBException, NoRowsAffectedDBException;
 	
+	/*
+	 * TODO
+	 */
 	public ArrayList<String> getAllCourseCodes() throws DBException, NoResultFoundDBException;
 	
+	/*
+	 * TODO
+	 */
 	public ArrayList<String> getAllCourseNames() throws DBException, NoResultFoundDBException;
 	
+	/*
+	 * TODO
+	 */
 	public CourseBean getCourse(String CourseCode) throws DBException, NoResultFoundDBException;
 }
