c830dd81c15461cf29f94e88cecf149776eea609
Rasmus Tilljander <tilljander.rasmus@gmail.com>
2014-08-11 15:35:48 +0200

    Added imports that was lost in merge

--- src/com/example/app_android/database/FavouriteCourseTable.java
+++ src/com/example/app_android/database/FavouriteCourseTable.java
@@ -7,6 +7,7 @@ import com.example.app_android.util.Utilities;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
